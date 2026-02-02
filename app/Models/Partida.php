7aaa5fec9a320810843f3378d082924ac5586f63
ogustavobrandao <sbrandao.gustavo7@gmail.com>
2026-02-02 15:21:14 -0300

    feat: adição das classes de para partida e ajustes na de usuário

---
+++ app/Models/Partida.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Partida extends Model
+{
+    /** @use HasFactory<\Database\Factories\PartidaFactory> */
+    use HasFactory;
+
+    protected $fillable = ['modo', 'dificuldade', 'status', 'criado_por', 'started_at', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsToMany(User::class, 'partida_user', 'user_id');
+    }
+}
---
+++ database/migrations/2025_12_11_231240_create_partidas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('partidas', function (Blueprint $table) {
+            $table->id();
+            $table->enum('modo', ['pve', 'pvp'])->default('pve');
+            $table->enum('dificuldade', ['facil', 'medio', 'dificil']);
+            $table->unsignedTinyInteger('board_width')->default(10);
+            $table->unsignedTinyInteger('board_height')->default(10);
+            $table->enum('status', ['aguardando', 'em_andamento', 'finalizada'])->default('aguardando');
+
+            $table->foreignId('criado_por')->constrained('users')->cascadeOnDelete();
+            $table->timestamp('started_at')->nullable();
+            $table->timestamp('finished_at')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('partidas');
+    }
+};
