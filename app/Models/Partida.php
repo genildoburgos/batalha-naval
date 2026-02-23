95eedc1c3769a00f1c37ba7c2b99025cb72f530d
ThaysonScript <thaysonguedes01@gmail.com>
2026-02-23 16:58:01 -0300

    refactor: atualizar relacionamentos e localização no model Partida

--- app/Models/Partida.php
+++ app/Models/Partida.php
@@ -14,6 +14,12 @@ class Partida extends Model
 
     public function user()
     {
-        return $this->belongsToMany(User::class, 'partida_user', 'user_id');
+        // return $this->belongsToMany(User::class, 'partida_user', 'user_id');
+        return $this->belongsToMany(User::class, 'partida_user', 'partida_id', 'user_id');
+    }
+
+    public function tabuleiros()
+    {
+        return $this->hasMany(Tabuleiro::class);
     }
 }
